6c97a1e2957e5ee2f564798d54705bf1379f1418
James <98734429+KinyuaJames@users.noreply.github.com>
2023-07-05 14:36:50 +0300

    Update face_recog.py

--- face_recog.py
+++ face_recog.py
@@ -1,16 +1,21 @@
 import cv2
 
+# reading the cascade xml
 face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 
+# reading the image
 img = cv2.imread('./test2.jpg')
 
+#converting the image to greyscale 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
+# getting the rectangle points for the image
 for(x,y,w,h) in faces:
     cv2.rectangle(img, (x,y), (x+w, y+h),(225,0,0), 2)
-    
+
+# output the image 
 cv2.imshow('img', img)
 cv2.waitKey()
 
